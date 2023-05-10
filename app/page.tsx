955bb905cb526729b23f33e88b51b6e5582e3aa5
pranjaljain0 <pranjaljain0697@gmail.com>
2023-05-10 15:14:47 -0400

    feat: Adding Side Navigation for the form

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { Component, ReactElement, useState } from 'react';
-import { handleNextStep, handlePrevStep } from "@/helpers/helper"
-
+import SideNav from '@/components/SideNav/SideNav';
 import Slide1 from '@/components/slides/slide1';
 import Slide2 from '@/components/slides/slide2';
 import Slide3 from '@/components/slides/slide3';
 import Slide4 from '@/components/slides/slide4';
 import Slide5 from '@/components/slides/slide5';
+import SlideButton from '@/components/Button/SlideButton';
+import { sidenav_data } from '@/data/sidenav';
+import { useState } from 'react';
 
 export default function Home() {
   const [step, setStep] = useState(0);
@@ -31,33 +32,7 @@ export default function Home() {
         <Slide5 />
         : null}
 
-      {step != 4 ?
-        <div className={
-          `flex 
-                lg:mt-16 
-                lg:py-10
-                lg:px-[7rem]
-                lg:bg-transparent
-                lg:absolute
-                lg:w-[49rem]
-                lg:border-t-0
-                fixed bottom-0 right-0 px-4 bg-white w-full py-3 mt-0 border-t-2
-                ${step > 0 ? 'justify-between' : 'justify-end'}`}
-        >
-          {step > 0 ?
-            <button type="button" onClick={() => handlePrevStep(setStep)} className="px-4 py-2 text-base transition rounded-md text-neutral-coolGray myFontBold hover:text-neutral-coolGray/50 hover:scale-90">
-              Go Back
-            </button>
-            : null}
-          <button type="submit"
-            onClick={() => { handleNextStep(setStep) }}
-            className={`w-32 h-12 px-4 py-2 text-base text-white rounded-md myFontRegular transition hover:scale-90
-                ${step == 3 ? 'bg-primary-purplishBlue hover:bg-primary-purplishBlue/90' : 'bg-primary-marineBlue hover:bg-primary-marineBlue/90'}
-                `}>
-            {step == 3 ? 'Confirm' : 'Next Step'}
-          </button>
-        </div>
-        : null}
+      <SlideButton setStep={setStep} step={step} />
     </>)
   }
 
@@ -68,7 +43,16 @@ export default function Home() {
           <div className="grid grid-cols-12 gap-8 lg:gap-6 md:gap-12">
             <nav className="hidden col-span-12 lg:col-span-4  bg-no-repeat bg-center h-[38rem] lg:block relative bg-[url(/images/bg-sidebar-desktop.svg)]">
               <div className="absolute top-[4rem] left-[4rem]">
-                <span>Side Nav</span>
+                {sidenav_data.map((step_data_item, index) => (
+                  <div key={index}>
+                    <SideNav
+                      step={step_data_item.step}
+                      title={step_data_item.title}
+                      message={step_data_item.message}
+                      state={index === step ? "active" : ""}
+                    />
+                  </div>
+                ))}
               </div>
             </nav>
             <div className="col-span-12 px-8 py-5 lg:pr-[7rem] lg:col-span-8">
